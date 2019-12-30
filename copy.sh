cc160a557b7f8ec4acdca6c38281a95ab8a35c7b
irac <iracding@tvunetworks.com>
2019-12-30 16:56:58 +0800

    update

--- copy.sh
+++ copy.sh
@@ -2,3 +2,7 @@
 \cp  -rf /home/tvu/SourceR/OrleansHelloWorld/src/SiloHost/bin/Debug/netcoreapp3.0/* /home/tvu/SourceR/Test/2
 \cp  -rf /home/tvu/SourceR/OrleansHelloWorld/src/SiloHost/bin/Debug/netcoreapp3.0/* /home/tvu/SourceR/Test/1
 \cp  -rf /home/tvu/SourceR/OrleansHelloWorld/src/OrleansClient/bin/Debug/netcoreapp3.0/* /home/tvu/SourceR/Test/Client
+dotnet /home/tvu/SourceR/Test/1/SiloHost.dll
+dotnet /home/tvu/SourceR/Test/2/SiloHost.dll
+dotnet /home/tvu/SourceR/Test/3/SiloHost.dll
+dotnet /home/tvu/SourceR/Test/Client/OrleansClient.dll
